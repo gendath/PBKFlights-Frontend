93f7a95cdddf80c7431f5e515c46226015f9d263
Perry Shelton <sellwithperry@gmail.com>
2022-04-21 22:30:21 -0700

    add navbar react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from "react";
+import {render} from "react-dom"
+import Menu from "./components/Menu"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/main.css"
+
+
+
+
+render(<Menu />, document.querySelector("#main"))
