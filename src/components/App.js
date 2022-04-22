2fad470628c476a5ab4c34c1f0b9a7408253a17a
Perry Shelton <sellwithperry@gmail.com>
2022-04-21 22:45:12 -0700

    initialized app structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react"
+import Menu from "./Menu"
+
+class App extends Component {
+    render(){
+        return (
+            <Menu />
+        )
+    }
+
+
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {render} from "react-dom"
-import Menu from "./components/Menu"
+import App from "./components/App"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/main.css"
 
 
 
 
-render(<Menu />, document.querySelector("#main"))
+render(<App />, document.querySelector("#main"))
